9a7233a7a760b127d3d32aa61dce99b2a9057731
JeannieFallon <jeanniefallon@gmail.com>
2018-12-24 16:53:08 -0500

    stub out rot13 flow

--- src/main/java/com/metatarsal/primavera/constants/PrimaveraConstants.java
+++ src/main/java/com/metatarsal/primavera/constants/PrimaveraConstants.java
@@ -11,9 +11,29 @@ public class PrimaveraConstants {
 
     }
 
+    //region Page Titles
     public static final String HOME_TITLE = "Home";
-    public static final String ROT13_TITLE = "ROT13";
-    public static final String CAESAR_TITLE = "Caesar";
-    public static final String VIGENERE_TITLE = "Vigenere";
+    public static final String ROT13_TITLE = "ROT13 Cipher";
+    public static final String CAESAR_TITLE = "Caesar Cipher";
+    public static final String VIGENERE_TITLE = "Vigenere Cipher";
+    //endregion
 
+    //region Ciphers
+    public static final String ROT13 = "ROT13";
+    public static final String CAESAR = "Caesar";
+    public static final String VIGENERE = "Vigenere";
+    //endregion
+
+    //region Params & Attributes
+    public static final String TITLE = "title";
+    public static final String INSTRUCTION = "instruction";
+    public static final String PLAINTEXT = "plaintext";
+    public static final String CIPHERTEXT = "ciphertext";
+    //endregion
+
+    //region Instructions & Labels (Display)
+    public static final String ENTER_PLAINTEXT = "Please enter the plaintext below.";
+    public static final String PLAINTEXT_LABEL = "Plaintext:";
+    public static final String CIPHERTEXT_LABEL = "Ciphertext:";
+    //endregion
 }
--- src/main/java/com/metatarsal/primavera/controllers/Rot13Controller.java
+++ src/main/java/com/metatarsal/primavera/controllers/Rot13Controller.java
@@ -4,6 +4,9 @@ import com.metatarsal.primavera.constants.PrimaveraConstants;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("rot13")
@@ -11,7 +14,19 @@ public class Rot13Controller {
 
     @RequestMapping(value = "")
     public String index(Model model) {
-        model.addAttribute("title", PrimaveraConstants.ROT13_TITLE);
+        model.addAttribute(PrimaveraConstants.INSTRUCTION, PrimaveraConstants.ENTER_PLAINTEXT);
+        model.addAttribute(PrimaveraConstants.TITLE, PrimaveraConstants.ROT13_TITLE);
         return "rot13/index";
+    }
+
+    @RequestMapping(value = "cipher", method = RequestMethod.POST)
+    public String getRot13Cipher(Model model, HttpServletRequest request) {
+
+        String plaintext = request.getParameter(PrimaveraConstants.PLAINTEXT);
+        String ciphertext = "This is test ciphertext";
+        model.addAttribute("plaintext", plaintext);
+        model.addAttribute("ciphertext", ciphertext);
+        model.addAttribute(PrimaveraConstants.TITLE, PrimaveraConstants.ROT13_TITLE);
+        return "rot13/cipher";
     }
 }
